c9736954b885ba3728f0014ab7dcb19476baf1fe
Cikica <aleksandar.winter@gmail.com>
2014-09-09 12:03:11 +0100

    Imrpoved the eloquent

--- library/eloquent/library/dropdown/dropdown.js
+++ library/eloquent/library/dropdown/dropdown.js
@@ -114,7 +114,7 @@ define({
 					option_state         = heard.state.option[name]
 					wrap.style.display   = "none"
 					notation.textContent = wrap.previousSibling.getAttribute("data-mark-closed")
-					text.textContent     = value
+					text.textContent     = option.getAttribute("data-dropdown-text")
 					option_state.value   = value
 					return heard
 				},
@@ -123,6 +123,7 @@ define({
 	},
 
 	define_body : function ( define ) {
+		var self = this
 		return { 
 			"class" : define.class_name.main,
 			child   : [
@@ -148,6 +149,11 @@ define({
 					"child"               : this.library.morphism.index_loop({
 						array   : define.with.option.choice,
 						else_do : function ( loop ) {
+							return loop.into.concat(self.define_option({
+								class_name : define.class_name,
+								name       : define.name,
+								option     : loop.indexed
+							}))
 							return loop.into.concat({
 								"class"               : define.class_name.option,
 								"data-dropdown-name"  : define.name,
@@ -159,6 +165,27 @@ define({
 				}
 			]
 		}
+	},
+
+	define_option : function ( define ) {
+
+		var definition
+		definition = {
+			"class"               : define.class_name.option,
+			"data-dropdown-name"  : define.name,
+		}
+
+		if ( define.option.value && define.option.text ) { 
+			definition["data-dropdown-value"] = define.option.value,
+			definition["data-dropdown-text"]  = define.option.text,
+			definition["text"]                = define.option.text
+		} else { 
+			definition["data-dropdown-value"] = define.option,
+			definition["data-dropdown-text"]  = define.option,
+			definition["text"]                = define.option
+		}
+
+		return definition
 	}
 	
 })
